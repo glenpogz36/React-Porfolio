df0e3d00730decd2d1f6c2db33094b8e4b4fb582
glen <glen_sale@yahoo.com>
2019-02-15 14:31:22 -0800

    imported error msg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './Header';
+import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
 
 
 function App() {
     return (
         <div>
             <Header />
-    
+            <Switch>
+
+                <Route component={Error404} />
+            </Switch>
         </div>
     );
 }
